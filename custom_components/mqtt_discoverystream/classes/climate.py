d5c69e9ef790a4871bbd5a5eac85a27aa485dcbc
Jamie Starke <git@jamiestarke.com>
2024-09-14 10:33:03 -0700

    Expose fan mode and swing mode

--- custom_components/mqtt_discoverystream/classes/climate.py
+++ custom_components/mqtt_discoverystream/classes/climate.py
@@ -6,26 +6,38 @@ from homeassistant.components.climate import (
     ATTR_CURRENT_TEMPERATURE,
     ATTR_HVAC_ACTION,
     ATTR_HVAC_MODES,
+    ATTR_FAN_MODE,
+    ATTR_FAN_MODES,
     ATTR_MAX_TEMP,
     ATTR_MIN_TEMP,
     ATTR_PRESET_MODE,
     ATTR_PRESET_MODES,
+    ATTR_SWING_MODE,
+    ATTR_SWING_MODES,
     ATTR_TARGET_TEMP_STEP,
     PRESET_NONE,
     SERVICE_SET_HVAC_MODE,
+    SERVICE_SET_FAN_MODE,
     SERVICE_SET_PRESET_MODE,
+    SERVICE_SET_SWING_MODE,
     SERVICE_SET_TEMPERATURE,
 )
 from homeassistant.components.mqtt.climate import (
     ATTR_HVAC_MODE,
     CONF_ACTION_TOPIC,
     CONF_CURRENT_TEMP_TOPIC,
+    CONF_FAN_MODE_COMMAND_TOPIC,
+    CONF_FAN_MODE_STATE_TOPIC,
+    CONF_FAN_MODE_LIST,
     CONF_MODE_COMMAND_TOPIC,
     CONF_MODE_LIST,
     CONF_MODE_STATE_TOPIC,
     CONF_PRESET_MODE_COMMAND_TOPIC,
     CONF_PRESET_MODE_STATE_TOPIC,
     CONF_PRESET_MODES_LIST,
+    CONF_SWING_MODE_LIST,
+    CONF_SWING_MODE_COMMAND_TOPIC,
+    CONF_SWING_MODE_STATE_TOPIC,
     CONF_TEMP_COMMAND_TOPIC,
     CONF_TEMP_MAX,
     CONF_TEMP_MIN,
@@ -40,7 +52,7 @@ from homeassistant.const import (
     Platform,
 )
 
-from ..const import ATTR_MODE_COMMAND, ATTR_PRESET_COMMAND, ATTR_TEMP_COMMAND
+from ..const import ATTR_MODE_COMMAND, ATTR_SWING_COMMAND, ATTR_FAN_COMMAND, ATTR_PRESET_COMMAND, ATTR_TEMP_COMMAND
 from ..utils import async_publish_base_attributes
 
 _LOGGER = logging.getLogger(__name__)
@@ -62,12 +74,20 @@ class Climate:
         config[CONF_MODE_COMMAND_TOPIC] = f"{mycommand}{ATTR_MODE_COMMAND}"
         config[CONF_MODE_LIST] = attributes[ATTR_HVAC_MODES]
         config[CONF_MODE_STATE_TOPIC] = f"{mybase}{ATTR_HVAC_MODE}"
-        preset_modes = attributes[ATTR_PRESET_MODES]
+        fan_modes = attributes.get(ATTR_FAN_MODES, None)
+        config[CONF_FAN_MODE_LIST] = fan_modes
+        config[CONF_FAN_MODE_COMMAND_TOPIC] = f"{mycommand}{ATTR_FAN_COMMAND}"
+        config[CONF_FAN_MODE_STATE_TOPIC] = f"{mybase}{ATTR_FAN_MODE}"
+        preset_modes = attributes.get(ATTR_PRESET_MODES, None)
         if PRESET_NONE in preset_modes:
             preset_modes.remove(PRESET_NONE)
         config[CONF_PRESET_MODES_LIST] = preset_modes
         config[CONF_PRESET_MODE_COMMAND_TOPIC] = f"{mycommand}{ATTR_PRESET_COMMAND}"
         config[CONF_PRESET_MODE_STATE_TOPIC] = f"{mybase}{ATTR_PRESET_MODE}"
+        swing_modes = attributes.get(ATTR_SWING_MODES, None)
+        config[CONF_SWING_MODE_LIST] = swing_modes
+        config[CONF_SWING_MODE_COMMAND_TOPIC] = f"{mycommand}{ATTR_SWING_COMMAND}"
+        config[CONF_SWING_MODE_STATE_TOPIC] = f"{mybase}{ATTR_SWING_MODE}"
         config[CONF_TEMP_COMMAND_TOPIC] = f"{mycommand}{ATTR_TEMP_COMMAND}"
         config[CONF_TEMP_STATE_TOPIC] = f"{mybase}{ATTR_TEMPERATURE}"
         if ATTR_TARGET_TEMP_STEP in attributes:
@@ -81,6 +101,8 @@ class Climate:
         await self._async_publish_attribute(new_state, mybase, ATTR_HVAC_ACTION, True)
         await self._async_publish_attribute(new_state, mybase, ATTR_CURRENT_TEMPERATURE)
         await self._async_publish_attribute(new_state, mybase, ATTR_PRESET_MODE, True)
+        await self._async_publish_attribute(new_state, mybase, ATTR_FAN_MODE, True)
+        await self._async_publish_attribute(new_state, mybase, ATTR_SWING_MODE, True)
         await self._async_publish_attribute(new_state, mybase, ATTR_TEMPERATURE)
 
         await async_publish_base_attributes(self._hass, new_state, mybase)
@@ -118,6 +140,14 @@ class Climate:
             self._async_handle_message,
         )
         await self._hass.components.mqtt.async_subscribe(
+            f"{command_topic}{Platform.CLIMATE}/+/{ATTR_FAN_COMMAND}",
+            self._async_handle_message,
+        )
+        await self._hass.components.mqtt.async_subscribe(
+            f"{command_topic}{Platform.CLIMATE}/+/{ATTR_SWING_COMMAND}",
+            self._async_handle_message,
+        )
+        await self._hass.components.mqtt.async_subscribe(
             f"{command_topic}{Platform.CLIMATE}/+/{ATTR_TEMP_COMMAND}",
             self._async_handle_message,
         )
@@ -143,6 +173,12 @@ class Climate:
         elif element == ATTR_PRESET_COMMAND:
             service_payload[ATTR_PRESET_MODE] = msg.payload
             service_name = SERVICE_SET_PRESET_MODE
+        elif element == ATTR_FAN_COMMAND:
+            service_payload[ATTR_FAN_MODE] = msg.payload
+            service_name = SERVICE_SET_FAN_MODE
+        elif element == ATTR_SWING_COMMAND:
+            service_payload[ATTR_SWING_MODE] = msg.payload
+            service_name = SERVICE_SET_SWING_MODE
         elif element == ATTR_TEMP_COMMAND:
             service_payload[ATTR_TEMPERATURE] = msg.payload
             service_name = SERVICE_SET_TEMPERATURE
